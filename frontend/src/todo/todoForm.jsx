551b47e9bf62592a4d2ed1c8dd70535c91918b67
Marcelo Dias <diasteixeira1990@gmail.com>
2022-11-18 20:19:07 -0300

    implementado funções: inserir, deletar e pesquisar

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,10 +6,12 @@ export default props => (
     <div role='form' className="todoForm">
         <Grid cols='12 9 10'>
             <input id='description' className="form-control"
-                placeholder="Adicione uma tarefa"></input>
+                placeholder="Adicione uma tarefa"onChange={props.handleChange} value={props.description}></input>
         </Grid>
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'onClick={props.handleAdd}></IconButton>
+            <IconButton btnStyle='primary' icon='plus'onClick={props.handleAdd}></IconButton>
+            <IconButton btnStyle='info' icon='search' onClick={props.handleSearch}></IconButton>
+            <IconButton btnStyle='default' icon='close' onClick={props.handleClear}></IconButton>
         </Grid>
     </div>
 )
